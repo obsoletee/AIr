011f425bb3dfd219c2ac7b6aa355076ce284e5e1
Iliaz Mamedov <ilzmamedov@gmail.com>
2024-12-09 01:42:24 +0300

    Nativewind was configured, and assets and fonts were added.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import { Slot, Stack } from 'expo-router';
+import { Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { SplashScreen, Stack } from 'expo-router';
+
+import '../global.css';
+import { useFonts } from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 const _layout = () => {
+  const [fontsLoaded, error] = useFonts({
+    'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
+    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-ExtraBold': require('../assets/fonts/Poppins-ExtraBold.ttf'),
+    'Poppins-ExtraLight': require('../assets/fonts/Poppins-ExtraLight.ttf'),
+    'Poppins-Light': require('../assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Semibold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Thin': require('../assets/fonts/Poppins-Thin.ttf'),
+  });
+
+  useEffect(() => {
+    if (error) {
+      throw error;
+    }
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return (
+      <View>
+        <Text>Something goes wrong with fonts</Text>
+      </View>
+    );
+  }
   return (
     <Stack>
       <Stack.Screen
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,13 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import { Link } from 'expo-router';
 import React from 'react';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>AIr</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">AIr</Text>
       <StatusBar barStyle="dark-content" />
-      <Link href="/profile" style={styles.link}>
+      <Link href="/profile" className="text-2xl">
         Go to the profile
       </Link>
     </View>
@@ -15,16 +15,3 @@ const App = () => {
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  link: {
-    color: 'blue',
-    fontSize: 24,
-  },
-});
